4b495b7a8c2e7f7795f7c3529e9fbf65066630b1
miza <miza@MacBook-Pro-de-miza.local>
2023-11-17 11:33:59 -0400

    creating pokemonCard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from "next";
-// You should use getServerSideProps when:
-// - Only if you need to pre-render a page whose data must be fetched at request time
 import { GetServerSideProps } from "next";
 import { Layout } from "../components/layouts";
 import { pokeApi } from "@/api";
 import { PokeminListResponse, SmallPokemon } from "@/interfaces";
-import { Card, Grid, Row, Text } from "@nextui-org/react";
+import { Grid } from "@nextui-org/react";
+import {PokemonCard} from '@/components/pokemon';
+
 
 interface Props {
   pokemons: SmallPokemon[];
@@ -15,27 +15,9 @@ const HomePage: NextPage<Props> = (props) => {
   console.log(props);
   return (
     <Layout title="Listado de Pokemon">
-      <Grid.Container gap={2} justify='flex-start'>
-        {props.pokemons.map(({ id, name }) => (
-          <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-            <Card hoverable clickable>
-              <Card.Body>
-                <Card.Image
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-                  width={100}
-                  height={100}
-                  alt={name}
-                 />
-              </Card.Body>
-              <Card.Footer>
-                <Row justify='space-between'>
-                  <Text transform="capitalize">{name}</Text>
-                  <Text>#{id}</Text>
-                </Row>
-              </Card.Footer>
-
-            </Card>
-          </Grid>
+      <Grid.Container gap={2} justify="flex-start">
+        {props.pokemons.map((poke) => (
+          <PokemonCard key={poke.id} pokemon={poke} />
         ))}
       </Grid.Container>
     </Layout>
